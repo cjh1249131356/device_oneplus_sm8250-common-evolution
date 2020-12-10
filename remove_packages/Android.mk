6db7e5289f2de6b558a94b5b5776d68a12eae752
cjh1249131356 <cjh1249131356@gmail.com>
2020-12-10 09:37:55 +0800

    sm8250-common: Remove more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci
+LOCAL_OVERRIDES_PACKAGES := NfcNci CarrierServices SafetyHubPrebuilt OPScreenRecord Maps VZWAPNLib Tycho NovaBugreportWrapper obdm_stub OBDM_Permissions OemDmTrigger MyVerizonServices Showcase SprintDM SprintHM AndroidAutoStubPrebuilt ConnMO DCMO USCCDM SoundAmplifierPrebuilt ScribePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
